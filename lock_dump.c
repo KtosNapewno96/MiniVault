152e3130aaca843ae0329221bcd02119b3675a63
KtosNapewno96 <KtosNapewno97@proton.me>
2026-05-09 14:38:06 +0200

    New file lock_dump.c

--- lock_dump.c
+++ lock_dump.c
@@ -1,14 +1,10 @@
 #include <windows.h>
 #include <aclapi.h>
 
-// Ta funkcja sprawi, że Task Manager powie "Odmowa dostępu"
 __declspec(dllexport) void secure_isolate_process()
 {
     HANDLE hProcess = GetCurrentProcess();
 
-    // Ustawiamy pusty (ale nie NULL!) DACL.
-    // Pusty DACL oznacza: "Nikt nie ma żadnych uprawnień do tego obiektu".
-    // Nawet PROCESS_QUERY_INFORMATION czy PROCESS_VM_READ zostaną zablokowane.
 
     PACL pEmptyDacl = (PACL)LocalAlloc(LPTR, sizeof(ACL));
     InitializeAcl(pEmptyDacl, sizeof(ACL), ACL_REVISION);
@@ -17,15 +13,15 @@ __declspec(dllexport) void secure_isolate_process()
         hProcess,
         SE_KERNEL_OBJECT,
         DACL_SECURITY_INFORMATION,
-        NULL,       // Owner
-        NULL,       // Group
-        pEmptyDacl, // Nasz pusty DACL
-        NULL        // SACL
+        NULL,
+        NULL,
+        pEmptyDacl,
+        NULL
     );
 
     if (res == ERROR_SUCCESS)
     {
-        // Sukces - od teraz proces jest w bunkrze
+        // Success
     }
 
     LocalFree(pEmptyDacl);
